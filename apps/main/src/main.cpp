8e7a5b763c4d37d91c5f250cba8751b64163a8ca
Henrik Strocka <henstr@hotmail.com>
2022-08-02 18:32:28 +0200

    Add etl and cleanup

--- apps/main/src/main.cpp
+++ apps/main/src/main.cpp
@@ -1,7 +1,7 @@
 #include <cstdint>
 #include <cstddef>
 
-#include "rp2040.hpp"
+#include "rp2040.h"
 
 void put(const uint32_t a1, const uint32_t a2)
 {
@@ -21,26 +21,137 @@ volatile void delay(const uint32_t a1)
     }
 }
 
-int main ( void )
+const uint32_t freq_125mhz = 125 * 1000000;
+const uint32_t freq_100mhz = 100 * 1000000;
+const uint32_t freq_12mhz = 12 * 1000000;
+const uint32_t freq_10mhz = 10 * 1000000;
+const uint32_t freq_6mhz = 6.5 * 1000000;
+const uint32_t freq_1mhz = 1 * 1000000;
+const uint32_t freq_100khz = 100000;
+const uint32_t freq_noe = 0x10000000;
+
+const uint32_t freq = freq_100khz;
+
+void blink()
 {
-    s_resets.reset.bits.io_bank_0 = 0;
-    s_resets.reset.bits.pads_bank_0 = 0;
+    s_sio.gpio_out_set = 1 << 25;
+    delay(freq);
 
-    while (!s_resets.reset_done.bits.io_bank_0 ||!s_resets.reset_done.bits.pads_bank_0) {}
+    s_sio.gpio_out_clr = 1 << 25;
+}
 
-    s_io_bank_0.gpio[25].control.bits.funcsel = 5;
+void blinkFase()
+{
+    s_sio.gpio_out_set = 1 << 25;
+    delay(10);
+
+    s_sio.gpio_out_clr = 1 << 25;
+}
+
+
+int main(void)
+{
+    s_xsoc.startup.delay = 47;
+
+    // Enable XOSC clock
+    s_xsoc.control.enable = 0xfab;
+
+    while (!s_xsoc.status.stable) {}
+
+    /*
+    delay(freq_100khz / 10);
+    blinkFase();
+    //s_clocks.sys.div.integer = 0xaa0 + 0x1;
+
+    //s_rsoc.control.freq_range = 0xaa0;
+    //s_rsoc.div.div = 0xaa0 + 1;
+
+    s_pll_sys.control.refdiv = 0x1;
+    s_pll_sys.fbdiv.div = 120;
+
+    s_pll_sys.pwr.pd = 1;
+    s_pll_sys.pwr.vcopd = 1;
+
+    blinkFase();
+
+    //while(!s_pll_sys.control.lock) { delay(1); };
+
+    delay(freq_100khz / 10);
+    blinkFase();
+
+    s_pll_sys.prim.postdiv1 = 6;
+    s_pll_sys.prim.postdiv2 = 5;
+
+    s_pll_sys.pwr.postdivpd = 1;
+
+    delay(freq_100khz / 10);
+
+    */
+
+    s_pll_sys.prim.postdiv1 = 6;
+    s_pll_sys.prim.postdiv2 = 5;
+    s_pll_sys.fbdiv.div = 120;
+
+    //s_pll_sys.control.refdiv = 0x1;
+
+    // Set clock source
+    s_clocks.sys.control.src = 0x0;
+    s_clocks.sys.control.auxsrc = 0x0;
+
+    while (!s_clocks.sys.selected) {}
+
+    s_clocks.sys.control.src = 0x1;
+    s_clocks.sys.div.integer = 0x1;
+
+    //uint32_t add = reinterpret_cast<uint32_t>(&s_rsoc.div);
+
+    s_resets.reset.io_bank_0 = 0;
+    s_resets.reset.pads_bank_0 = 0;
+
+    while (!s_resets.reset_done.io_bank_0 || !s_resets.reset_done.pads_bank_0) {}
+
+    s_io_bank_0.gpio[25].control.funcsel = 5;
     s_sio.gpio_oe_set = 1 << 25;
     s_sio.gpio_out_set = 1 << 25;
 
-    while(true)
+    /*
+    if (add == 0x40060000 + 0x10 && false)
     {
-        volatile auto d = 0x100000;
+        blink();
+        delay(freq / 2);
+
+        blink();
+        delay(freq / 2);
+
+        blink();
+        delay(freq / 2);
+
+        blink();
+        delay(freq / 2);
+    }
+    */
+
+    /*
+    for (size_t i = 0; i < 10; i++)
+    {
+        blink();
+
+        if (i == s_clocks.sys.control.auxsrc)
+        {
+            delay(0x100 / 3);
+            blink();
+        }
+
+        delay(0x100 * 10);
+    }
+    */
+    while (true)
+    {
+        s_sio.gpio_out_clr = 1 << 25;
+        delay(freq);
 
         s_sio.gpio_out_set = 1 << 25;
-        delay(d);
-
-        s_sio.gpio_out_clr = 1 << 25;
-        delay(d);
+        delay(freq);
     }
     return(0);
 }
--- libs/pico/inc/rp2040_regs.h
+++ libs/pico/inc/rp2040_regs.h
@@ -3,88 +3,117 @@
 #include <cstdint>
 
 struct SIO {
-	uint32_t cpuid;
-	uint32_t gpio_in;
-	uint32_t gpio_hi_in;
-	uint32_t reserved;
-	uint32_t gpio_out;
-	uint32_t gpio_out_set;
-	uint32_t gpio_out_clr;
-	uint32_t gpio_out_xor;
-	uint32_t gpio_oe;
-	uint32_t gpio_oe_set;
-	uint32_t gpio_oe_clr;
-	uint32_t gpio_oe_xor;
-	uint32_t gpio_hi_out;
-	uint32_t gpio_hi_out_set;
-	uint32_t gpio_hi_out_clr;
-	uint32_t gpio_hi_out_xor;
-	uint32_t gpio_hi_oe;
-	uint32_t gpio_hi_oe_set;
-	uint32_t gpio_hi_oe_clr;
-	uint32_t gpio_hi_oe_xor;
+    uint32_t cpuid;
+    uint32_t gpio_in;
+    uint32_t gpio_hi_in;
+    uint32_t reserved;
+    uint32_t gpio_out;
+    uint32_t gpio_out_set;
+    uint32_t gpio_out_clr;
+    uint32_t gpio_out_xor;
+    uint32_t gpio_oe;
+    uint32_t gpio_oe_set;
+    uint32_t gpio_oe_clr;
+    uint32_t gpio_oe_xor;
+    uint32_t gpio_hi_out;
+    uint32_t gpio_hi_out_set;
+    uint32_t gpio_hi_out_clr;
+    uint32_t gpio_hi_out_xor;
+    uint32_t gpio_hi_oe;
+    uint32_t gpio_hi_oe_set;
+    uint32_t gpio_hi_oe_clr;
+    uint32_t gpio_hi_oe_xor;
 };
 
 struct Clocks
 {
-    union
+    struct
     {
-        uint32_t regs;
-        struct
+        union
         {
-            uint32_t : 5;
-            uint32_t auxsrc : 4;
-            uint32_t : 1;
-            uint32_t kill : 1;
-            uint32_t enable : 1;
-            uint32_t dc50 : 1;
-            uint32_t : 3;
-            uint32_t phase : 2;
-            uint32_t : 2;
-            uint32_t nudge : 1;
-        } bits;
-    } control;
+            uint32_t : 32;
+            struct
+            {
+                uint32_t : 5;
+                uint32_t auxsrc : 4;
+                uint32_t : 1;
+                uint32_t kill : 1;
+                uint32_t enable : 1;
+                uint32_t dc50 : 1;
+                uint32_t : 3;
+                uint32_t phase : 2;
+                uint32_t : 2;
+                uint32_t nudge : 1;
+            };
+        } control;
+
+        union
+        {
+            uint32_t : 32;
+            struct
+            {
+                uint32_t frac : 8;
+                uint32_t integer : 24;
+            };
+        } div;
+
+        uint32_t selected;
+    } gpio[4];
+
+    struct
+    {
+        union
+        {
+            uint32_t : 32;
+            struct
+            {
+                uint32_t src : 2;
+                uint32_t : 3;
+                uint32_t auxsrc : 2;
+            };
+        } control;
+
+        union
+        {
+            uint32_t : 32;
+            struct
+            {
+                uint32_t : 8;
+                uint32_t integer : 2;
+            };
+        } div;
+
+        uint32_t selected;
+    } ref;
+
+    struct
+    {
+        union
+        {
+            uint32_t : 32;
+            struct
+            {
+                uint32_t src : 1;
+                uint32_t : 4;
+                uint32_t auxsrc : 3;
+            };
+        } control;
+
+        union
+        {
+            uint32_t : 32;
+            struct
+            {
+                uint32_t frac : 8;
+                uint32_t integer : 24;
+            };
+        } div;
+
+        uint32_t selected;
+    } sys;
 };
 
 struct IO_BANK_0 {
-    /*
-    struct {
-        union {
-            uint32_t regs;
-            struct {
-                uint32_t : 8;
-                uint32_t outfromperi : 1;
-                uint32_t outtopad : 1;
-                uint32_t : 2;
-                uint32_t oefromperi : 1;
-                uint32_t oetopad : 1;
-                uint32_t : 2;
-                uint32_t infrompad : 1;
-                uint32_t : 1;
-                uint32_t intoperi : 1;
-                uint32_t : 4;
-                uint32_t irqfrompad : 1;
-                uint32_t : 1;
-                uint32_t irqtoproc : 1;
-            } bits;
-        } status;
-        union {
-            uint32_t regs;
-            struct {
-                uint32_t funcsel : 5;
-                uint32_t : 3;
-                uint32_t outover : 2;
-                uint32_t : 2;
-                uint32_t oeover : 2;
-                uint32_t : 2;
-                uint32_t inover : 2;
-                uint32_t : 10;
-                uint32_t irqover : 2;
-            } bits;
-        } control;
-    } gpio[30];
-    */
-
     struct {
         union {
             uint32_t : 32;
@@ -103,7 +132,7 @@ struct IO_BANK_0 {
                 uint32_t irqfrompad : 1;
                 uint32_t : 1;
                 uint32_t irqtoproc : 1;
-            } bits;
+            };
         } status;
         union {
             uint32_t : 32;
@@ -124,7 +153,7 @@ struct IO_BANK_0 {
 
 struct RESETS {
     union {
-        uint32_t regs;
+        uint32_t : 32;
         struct {
             uint32_t adc : 1;
             uint32_t busctrl : 1;
@@ -151,10 +180,10 @@ struct RESETS {
             uint32_t uart_0 : 1;
             uint32_t uart_1 : 1;
             uint32_t usbctrl : 1;
-        } bits;
+        };
     } reset;
     union {
-        uint32_t regs;
+        uint32_t : 32;
         struct {
             uint32_t adc : 1;
             uint32_t busctrl : 1;
@@ -181,10 +210,10 @@ struct RESETS {
             uint32_t uart_0 : 1;
             uint32_t uart_1 : 1;
             uint32_t usbctrl : 1;
-        } bits;
+        };
     } wdsel;
     union {
-        uint32_t regs;
+        uint32_t : 32;
         struct {
             uint32_t adc : 1;
             uint32_t busctrl : 1;
@@ -211,11 +240,174 @@ struct RESETS {
             uint32_t uart_0 : 1;
             uint32_t uart_1 : 1;
             uint32_t usbctrl : 1;
-        } bits;
+        };
     } reset_done;
 };
 
-//volatile SIO s_sio __attribute__((section(".sio_regs")));
-//volatile IO_BANK_0 s_io_bank_0 __attribute__((section(".io_bank_0_regs")));
+struct RSOC
+{
+    union
+    {
+        uint32_t : 32;
+        struct
+        {
+            uint32_t freq_range : 12;
+            uint32_t enable : 12;
+        };
+    } control;
+
+    union
+    {
+        uint32_t : 32;
+        struct
+        {
+            uint32_t ds0 : 3;
+            uint32_t : 1;
+            uint32_t ds1 : 3;
+            uint32_t : 1;
+            uint32_t ds2 : 3;
+            uint32_t : 1;
+            uint32_t ds3 : 3;
+            uint32_t : 1;
+            uint32_t passwd : 16;
+        };
+    } freqa;
+
+    union
+    {
+        uint32_t : 32;
+        struct
+        {
+            uint32_t ds4 : 3;
+            uint32_t : 1;
+            uint32_t ds5 : 3;
+            uint32_t : 1;
+            uint32_t ds6 : 3;
+            uint32_t : 1;
+            uint32_t ds7 : 3;
+            uint32_t : 1;
+            uint32_t passwd : 16;
+        };
+    } freqb;
+
+    uint32_t dormant;
+
+    union
+    {
+        uint32_t : 32;
+        struct
+        {
+            uint32_t div : 12;
+        };
+    } div;
+};
+
+struct XSOC
+{
+    union
+    {
+        uint32_t : 32;
+        struct
+        {
+            uint32_t freq_range : 12;
+            uint32_t enable : 12;
+        };
+    } control;
+
+    union
+    {
+        uint32_t : 32;
+        struct
+        {
+            uint32_t freq_range : 2;
+            uint32_t : 10;
+            uint32_t enabled : 1;
+            uint32_t : 11;
+            uint32_t badwrite : 1;
+            uint32_t : 6;
+            uint32_t stable : 1;
+        };
+    } status;
+
+    uint32_t dormant;
+
+    union
+    {
+        uint32_t : 32;
+        struct
+        {
+            uint32_t delay : 14;
+            uint32_t : 5;
+            uint32_t x4 : 1;
+        };
+    } startup;
+
+    union
+    {
+        uint32_t : 32;
+        struct
+        {
+            uint32_t counter : 8;
+        };
+    } count;
+};
+
+struct PLL
+{
+    union
+    {
+        uint32_t : 32;
+        struct
+        {
+            uint32_t refdiv : 6;
+            uint32_t : 2;
+            uint32_t bypass : 1;
+            uint32_t : 22;
+            uint32_t lock : 1;
+        };
+    } control;
+
+    union
+    {
+        uint32_t : 32;
+        struct
+        {
+            uint32_t pd : 1;
+            uint32_t : 1;
+            uint32_t dsmpd : 1;
+            uint32_t postdivpd : 1;
+            uint32_t : 1;
+            uint32_t vcopd : 1;
+        };
+    } pwr;
+
+    union
+    {
+        uint32_t : 32;
+        struct
+        {
+            uint32_t div : 12;
+        };
+    } fbdiv;
+
+    union
+    {
+        uint32_t : 32;
+        struct
+        {
+            uint32_t : 12;
+            uint32_t postdiv2 : 3;
+            uint32_t : 1;
+            uint32_t postdiv1 : 3;
+        };
+    } prim;
+};
+
+volatile SIO s_sio __attribute__((section(".sio_regs")));
+volatile IO_BANK_0 s_io_bank_0 __attribute__((section(".io_bank_0_regs")));
 volatile RESETS s_resets __attribute__((section(".reset_regs")));
 volatile Clocks s_clocks __attribute__((section(".clock_regs")));
+volatile RSOC s_rsoc __attribute__((section(".rsoc_regs")));
+volatile XSOC s_xsoc __attribute__((section(".xsoc_regs")));
+volatile PLL s_pll_sys __attribute__((section(".pll_sys_regs")));
+volatile PLL s_pll_usb __attribute__((section(".pll_sys_usb")));
