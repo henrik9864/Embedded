da5e31f9466409aedd76da8e0f98b9a09ee0758d
Henrik Strocka <henstr@hotmail.com>
2022-10-27 19:16:15 +0200

    Add tools folder and remove unused stuff

--- apps/main/src/main.cpp
+++ apps/main/src/main.cpp
@@ -59,21 +59,12 @@ const uint32_t freq = freq_48mhz;
 void sleep(const uint32_t ns)
 {
     delay3((ns / 1000000000) * freq);
-    //delay3(ns * 0.048f);
 }
 
 void blink()
 {
     s_sio.gpio_out_set = 1 << 25;
     delay(freq);
-
-    s_sio.gpio_out_clr = 1 << 25;
-}
-
-void blinkFase()
-{
-    s_sio.gpio_out_set = 1 << 25;
-    delay(10);
 
     s_sio.gpio_out_clr = 1 << 25;
 }
@@ -137,16 +128,6 @@ void init_gpio()
     while (!s_resets.reset_done.io_bank_0 || !s_resets.reset_done.pads_bank_0) {}
 }
 
-void init_uart(uint32_t baud)
-{
-    s_resets.reset.uart_0 = 1;
-
-    s_resets.reset.uart_0 = 0;
-    while (!s_resets.reset_done.uart_0) {}
-
-    baud = (freq_133mhz * 4) / baud;
-}
-
 void setup_clocks()
 {
     // Disable resus that my have been enabled.
@@ -183,10 +164,10 @@ void setup_pio()
     //s_pio_0.fdebug.txstall = 0x0;
 
     // Load blink PIO program
-    //s_pio_0.instr_mem[0] = 0xe081;
-    //s_pio_0.instr_mem[1] = 0xfe01;
-    //s_pio_0.instr_mem[2] = 0xfd00;
-    //s_pio_0.instr_mem[3] = 0x0001;
+    s_pio_0.instr_mem[0] = 0xe081;
+    s_pio_0.instr_mem[1] = 0xfe01;
+    s_pio_0.instr_mem[2] = 0xfd00;
+    s_pio_0.instr_mem[3] = 0x0001;
 }
 
 void setup_sm0()
@@ -298,142 +279,3 @@ int main(void)
     }
     return(0);
 }
-
-/*
-int main(void)
-{
-    s_resets.reset.pll_sys = 0;
-    s_resets.reset.io_bank_0 = 0;
-    s_resets.reset.pads_bank_0 = 0;
-
-    while (!s_resets.reset_done.io_bank_0 || !s_resets.reset_done.pads_bank_0 || !s_resets.reset_done.pll_sys) {}
-
-    s_clocks.sys_resus.control.enable = 0x0;
-
-    const bool enable_xsoc = true;
-
-    if constexpr(enable_xsoc)
-    {
-        s_xsoc.control.freq_range = 0xaab;
-        //s_xsoc.startup.x4 = 47;
-        s_xsoc.startup.delay = 47;
-
-        // Enable XOSC clock
-        s_xsoc.control.enable = 0xfab; // Enable
-
-        while (!s_xsoc.status.stable  || !s_xsoc.status.enabled) {}
-    }
-    else
-    {
-        s_xsoc.control.enable = 0xd1e; // Disable
-    }
-
-    s_clocks.ref.control.src = 0x2;
-    s_clocks.sys.control.src = 0x0;
-
-    s_resets.reset.pll_sys = 0;
-    while (!s_resets.reset_done.pll_sys) {}
-
-    s_pll_sys.control.refdiv = 0x1;
-    s_pll_sys.fbdiv.div = 40;
-    s_pll_sys.prim.postdiv1 = 4;
-    s_pll_sys.prim.postdiv2 = 1;
-
-    s_pll_sys.pwr.vcopd = 0x1;
-    s_pll_sys.pwr.pd = 0x1;
-    while (!s_pll_sys.control.lock) {}
-    s_pll_sys.pwr.postdivpd = 0x1;
-
-    s_clocks.sys.control.auxsrc = 0x0;
-    s_clocks.sys.control.src = 0x0;
-
-    s_clocks.sys.control.src = 0x0;
-    s_clocks.sys.control.src = 0x0;
-
-    while (!s_clocks.sys.selected) {}
-    while (!s_clocks.ref.selected) {}
-
-    //Turn on PLL for 48mhz
-    s_pll_usb.control.refdiv = 0x1;
-    s_pll_usb.fbdiv.div = 64;
-
-    s_pll_usb.pwr.pd = 0x0;
-    s_pll_usb.pwr.vcopd = 0x0;
-
-    while (s_pll_usb.control.lock) {}
-
-    s_pll_usb.prim.postdiv1 = 4;
-    s_pll_usb.prim.postdiv2 = 4;
-
-    s_pll_usb.pwr.postdivpd = 0x0;
-    //s_pll_sys.pwr.dsmpd = 0x0;
-
-    // Set clock source
-    s_clocks.sys.control.auxsrc = 0x0; // PLL
-    //s_clocks.sys.control.auxsrc = 0x3; // XSOC
-
-    while (!s_clocks.sys.selected) {}
-
-    //s_clocks.sys.control.src = 0x0;
-    s_clocks.sys.control.src = 0x1;
-    //s_clocks.ref.control.src = 0x2;
-
-    while (!s_clocks.sys.selected) {}
-
-    s_clocks.sys.div.integer = 0x1;
-    //s_clocks.ref.div.integer = 0x1;
-
-    //delay(100000 * 10);
-
-    //s_clocks.sys.control.auxsrc = 0x0;
-
-    // Enable XSOC as GPIO0
-    s_clocks.gpio[0].control.auxsrc = 0x5;
-    s_clocks.gpio[0].control.enable = 0x1;
-    //s_clocks.gpio[0].div.integer = 0x10000;
-
-    s_io_bank_0.gpio[21].control.funcsel = 8; // Set Pin 21 as clock out
-
-    s_io_bank_0.gpio[25].control.funcsel = 5;
-    s_sio.gpio_oe_set = 1 << 25;
-    s_sio.gpio_out_set = 1 << 25;
-
-    uint32_t add = reinterpret_cast<uint32_t>(&s_xsoc.dormant);
-    if (add == 0x40024000 + 0x09)
-    {
-        blink();
-        delay(freq / 4);
-
-        blink();
-        delay(freq / 4);
-
-        blink();
-        delay(freq / 4);
-
-        blink();
-        delay(freq / 4);
-    }
-
-    for (size_t i = 0; i < 10; i++)
-    {
-        blink();
-
-        if (i == s_clocks.sys.control.auxsrc)
-        {
-            delay(0x100 / 3);
-            blink();
-        }
-
-        delay(0x100 * 10);
-    }
-    while (true)
-    {
-        s_sio.gpio_out_set = 1 << 25;
-        delay2(freq);
-
-        s_sio.gpio_out_clr = 1 << 25;
-        delay2(freq);
-    }
-    return(0);
-}
-*/
