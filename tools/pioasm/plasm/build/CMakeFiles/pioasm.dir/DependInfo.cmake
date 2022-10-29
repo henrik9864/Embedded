75472358dcb9193a9096bed6e15ef377a491aa4b
henrik9864 <henstr@hotmail.com>
2022-10-29 16:16:06 +0200

    readded pioasm

---
+++ tools/pioasm/plasm/build/CMakeFiles/pioasm.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/henrik/Documents/cpp/pioasm/src/c_output.cpp" "CMakeFiles/pioasm.dir/src/c_output.cpp.o" "gcc" "CMakeFiles/pioasm.dir/src/c_output.cpp.o.d"
+  "/home/henrik/Documents/cpp/pioasm/src/c_sdk_output.cpp" "CMakeFiles/pioasm.dir/src/c_sdk_output.cpp.o" "gcc" "CMakeFiles/pioasm.dir/src/c_sdk_output.cpp.o.d"
+  "/home/henrik/Documents/cpp/pioasm/src/lexer.cpp" "CMakeFiles/pioasm.dir/src/lexer.cpp.o" "gcc" "CMakeFiles/pioasm.dir/src/lexer.cpp.o.d"
+  "/home/henrik/Documents/cpp/pioasm/src/main.cpp" "CMakeFiles/pioasm.dir/src/main.cpp.o" "gcc" "CMakeFiles/pioasm.dir/src/main.cpp.o.d"
+  "/home/henrik/Documents/cpp/pioasm/src/parser.cpp" "CMakeFiles/pioasm.dir/src/parser.cpp.o" "gcc" "CMakeFiles/pioasm.dir/src/parser.cpp.o.d"
+  "/home/henrik/Documents/cpp/pioasm/src/pio_assembler.cpp" "CMakeFiles/pioasm.dir/src/pio_assembler.cpp.o" "gcc" "CMakeFiles/pioasm.dir/src/pio_assembler.cpp.o.d"
+  "/home/henrik/Documents/cpp/pioasm/src/pio_disassembler.cpp" "CMakeFiles/pioasm.dir/src/pio_disassembler.cpp.o" "gcc" "CMakeFiles/pioasm.dir/src/pio_disassembler.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
